41703902e3c1f8aac083002098ce5d23056da80f
Md Josim <josimhossain842@gmail.com>
2024-08-10 10:52:47 +0600

    Bugs fixing

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
-import About from '@/components/About';
-import Header from '@/components/Header';
-import Project from '@/components/Project';
 
-import Youtube from '@/components/Youtube';
-import Skill from '@/components/Skill';
-import Education from '@/components/Education';
-import Activities from '@/components/Activities';
 // import Gallery from '@/components/Gallery';
+
+import About from "@/components/About";
+import Activities from "@/components/Activities";
+import Education from "@/components/Education";
+import Header from "@/components/Header";
+import Project from "@/components/Project";
+import Skill from "@/components/Skill";
+import Youtube from "@/components/Youtube";
 
 export default function Home() {
   return (
